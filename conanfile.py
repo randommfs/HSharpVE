1321b76032b95febabbc4d282c2a8905f8515edf
HyperWinX <artmsmth@gmail.com>
2025-09-14 20:04:43 +0400

    Project reboot

--- conanfile.py
+++ conanfile.py
@@ -1,26 +1,46 @@
 from conan import ConanFile
-from conan.tools.cmake import cmake_layout
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
 
-def match_versions(dep: str) -> str:
-    deps = {
-        'plog': '1.1.10',
-        'gtest': '1.14.0',
-        'nlohmann_json': '3.11.3'
-    }
 
-    return deps[dep]
+class HSharpVERecipe(ConanFile):
+    name = "HSharpVE"
+    version = "0.1"
+    package_type = "application"
 
-class HSharp(ConanFile):
-    # profile
-    version = '0.0a'
-    generators = ['CMakeToolchain', 'CMakeDeps']
-    settings = 'os', 'compiler', 'build_type', 'arch'
+    license = "GPLv3"
+    author = "HyperWinX, AshFungor"
+    url = "https://github.com/randommfs/HSharpVE"
+    description = "Simple programming language with a stack based VM"
 
-    def requirements(self):
-        for req in [f'{dep}/{match_versions(dep)}' for dep in [
-            'plog', 'gtest', 'nlohmann_json'
-        ]]:
-            self.requires(req)
+    settings = "os", "compiler", "build_type", "arch"
+
+    requires = [
+        "gtest/1.14.0",
+        "boost/1.87.0",
+        "spdlog/1.15.0",
+        "argparse/3.2",
+        "eternal/1.0.1",
+        "fmt/11.0.2"
+    ]
 
     def layout(self):
         cmake_layout(self)
+
+    def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()
+        tc = CMakeToolchain(self)
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+
+    
+
+    
--- include/VersionInfo.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#define HSHARP_MAINTAINER "HyperWin"
-#define HSHARP_NAME "HSharpVE_Engine"
-#define HSHARP_VERSION "0.1a"
