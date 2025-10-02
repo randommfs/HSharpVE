9223a866ba45d48accec0daf8c3823159e512689
HyperWinX <artmsmth@gmail.com>
2025-10-02 12:32:55 +0400

    More tests

--- tests/Parser/ParseExpr.cpp
+++ tests/Parser/ParseExpr.cpp
@@ -157,3 +157,46 @@ TEST(Parser, ParseBinExprDivParen) {
   ASSERT_EQ(lit->int_lit.type, HVE::TokenType::INT_LITERAL);
   ASSERT_EQ(lit->int_lit.lexeme, "4");
 }
+
+TEST(Parser, ParseBinExprDiffPrec) {
+  std::string src = "(5 - 4) * 8";
+  auto tokens = HVE::Tokenizer{src}.Tokenize();
+  auto parser = HVE::Parser::Parser(std::move(tokens));
+
+  std::optional<HVE::Parser::NodeExpr*> expr = parser.ParseExpr();
+  ASSERT_TRUE(expr.has_value());
+
+  ASSERT_TRUE(std::holds_alternative<HVE::Parser::NodeBinExpr*>(expr.value()->var));
+  auto bexpr= std::get<HVE::Parser::NodeBinExpr*>(expr.value()->var);
+  ASSERT_TRUE(std::holds_alternative<HVE::Parser::NodeBinExprMulti*>(bexpr->var));
+  auto bexprmul = std::get<HVE::Parser::NodeBinExprMulti*>(bexpr->var);
+
+  ASSERT_TRUE(std::holds_alternative<HVE::Parser::NodeTerm*>(bexprmul->lhs->var));
+  auto term = std::get<HVE::Parser::NodeTerm*>(bexprmul->lhs->var);
+  auto termparen = std::get<HVE::Parser::NodeTermParen*>(term->term);
+  ASSERT_TRUE(std::holds_alternative<HVE::Parser::NodeBinExpr*>(termparen->expr->var));
+  auto binexpr = std::get<HVE::Parser::NodeBinExpr*>(termparen->expr->var);
+  ASSERT_TRUE(std::holds_alternative<HVE::Parser::NodeBinExprSub*>(binexpr->var));
+  auto bexprdiv = std::get<HVE::Parser::NodeBinExprSub*>(binexpr->var);
+
+  ASSERT_TRUE(std::holds_alternative<HVE::Parser::NodeTerm*>(bexprdiv->lhs->var));
+  term = std::get<HVE::Parser::NodeTerm*>(bexprdiv->lhs->var);
+  ASSERT_TRUE(std::holds_alternative<HVE::Parser::NodeTermIntLit*>(term->term));
+  auto lit = std::get<HVE::Parser::NodeTermIntLit*>(term->term);
+  ASSERT_EQ(lit->int_lit.type, HVE::TokenType::INT_LITERAL);
+  ASSERT_EQ(lit->int_lit.lexeme, "5");
+
+  ASSERT_TRUE(std::holds_alternative<HVE::Parser::NodeTerm*>(bexprdiv->rhs->var));
+  term = std::get<HVE::Parser::NodeTerm*>(bexprdiv->rhs->var);
+  ASSERT_TRUE(std::holds_alternative<HVE::Parser::NodeTermIntLit*>(term->term));
+  lit = std::get<HVE::Parser::NodeTermIntLit*>(term->term);
+  ASSERT_EQ(lit->int_lit.type, HVE::TokenType::INT_LITERAL);
+  ASSERT_EQ(lit->int_lit.lexeme, "4");
+
+  ASSERT_TRUE(std::holds_alternative<HVE::Parser::NodeTerm*>(bexprmul->rhs->var));
+  term = std::get<HVE::Parser::NodeTerm*>(bexprmul->rhs->var);
+  ASSERT_TRUE(std::get<HVE::Parser::NodeTermIntLit*>(term->term));
+  lit = std::get<HVE::Parser::NodeTermIntLit*>(term->term);
+  ASSERT_EQ(lit->int_lit.type, HVE::TokenType::INT_LITERAL);
+  ASSERT_EQ(lit->int_lit.lexeme, "8");
+}
